6b063bf3aade39e31375181a4062db7a1801082b
Adam Paquette <acpaquette@usgs.gov>
2020-12-17 09:53:10 -0700

    Updated Thm2isis tests to use a unique pointer for histogram

--- isis/tests/FunctionalTestsThm2isis.cpp
+++ isis/tests/FunctionalTestsThm2isis.cpp
@@ -84,82 +84,71 @@ TEST_F(TempTestingFiles, FunctionalTestsThm2isisIr) {
   EXPECT_EQ(int(kernel["NaifFrameCode"]), -53031);
 
 
-  Histogram* hist (cube->histogram(0));
+  std::unique_ptr<Histogram> hist (cube->histogram(0));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.00050067109171232009);
   EXPECT_DOUBLE_EQ(hist->Sum(), 16.021474934794242);
   EXPECT_EQ(hist->ValidPixels(), 32000);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 0.00012358176146689913);
-  delete hist;
 
-  hist = cube->histogram(1);
+  hist.reset(cube->histogram(1));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.00044544746567225956);
   EXPECT_DOUBLE_EQ(hist->Sum(), 1.4254318901512306);
   EXPECT_EQ(hist->ValidPixels(), 3200);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 2.1793759250527171e-05);
-  delete hist;
 
-  hist = cube->histogram(2);
+  hist.reset(cube->histogram(2));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.00044025198634699335);
   EXPECT_DOUBLE_EQ(hist->Sum(), 1.4088063563103788);
   EXPECT_EQ(hist->ValidPixels(), 3200);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 2.2115190306691905e-05);
-  delete hist;
 
-  hist = cube->histogram(3);
+  hist.reset(cube->histogram(3));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.00050481051051974645);
   EXPECT_DOUBLE_EQ(hist->Sum(), 1.6153936336631887);
   EXPECT_EQ(hist->ValidPixels(), 3200);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 2.7345479518019523e-05);
-  delete hist;
 
-  hist = cube->histogram(4);
+  hist.reset(cube->histogram(4));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.00050981284157387559);
   EXPECT_DOUBLE_EQ(hist->Sum(), 1.631401093036402);
   EXPECT_EQ(hist->ValidPixels(), 3200);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 2.6970020502410073e-05);
-  delete hist;
 
-  hist = cube->histogram(5);
+  hist.reset(cube->histogram(5));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.00053160175853918191);
   EXPECT_DOUBLE_EQ(hist->Sum(), 1.7011256273253821);
   EXPECT_EQ(hist->ValidPixels(), 3200);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 2.6931465772319629e-05);
-  delete hist;
 
-  hist = cube->histogram(6);
+  hist.reset(cube->histogram(6));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.00057632195762380434);
   EXPECT_DOUBLE_EQ(hist->Sum(), 1.8442302643961739);
   EXPECT_EQ(hist->ValidPixels(), 3200);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 2.7766022638966021e-05);
-  delete hist;
 
-  hist = cube->histogram(7);
+  hist.reset(cube->histogram(7));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.00059891219130804531);
   EXPECT_DOUBLE_EQ(hist->Sum(), 1.9165190121857449);
   EXPECT_EQ(hist->ValidPixels(), 3200);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 2.9141699404527645e-05);
-  delete hist;
 
-  hist = cube->histogram(8);
+  hist.reset(cube->histogram(8));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.00060846633206892878);
   EXPECT_DOUBLE_EQ(hist->Sum(), 1.947092262620572);
   EXPECT_EQ(hist->ValidPixels(), 3200);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 2.9540049483839706e-05);
-  delete hist;
 
-  hist = cube->histogram(9);
+  hist.reset(cube->histogram(9));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.00060713443390341131);
   EXPECT_DOUBLE_EQ(hist->Sum(), 1.942830188490916);
   EXPECT_EQ(hist->ValidPixels(), 3200);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 2.5376997469565179e-05);
-  delete hist;
 
-  hist = cube->histogram(10);
+  hist.reset(cube->histogram(10));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.00018395143956695392);
   EXPECT_DOUBLE_EQ(hist->Sum(), 0.58864460661425255);
   EXPECT_EQ(hist->ValidPixels(), 3200);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 4.7876398382339314e-06);
-  delete hist;
 }
 
 TEST_F(TempTestingFiles, FunctionalTestsThm2isisVis) {
@@ -263,89 +252,77 @@ TEST_F(TempTestingFiles, FunctionalTestsThm2isisVis) {
   kernel = evenLabel->findGroup("Kernels", Pvl::Traverse);
   EXPECT_EQ(int(kernel["NaifFrameCode"]), -53032);
 
-  Histogram* hist (oddCube->histogram(0));
+  std::unique_ptr<Histogram> hist (oddCube->histogram(0));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.0032050829203333175);
   EXPECT_DOUBLE_EQ(hist->Sum(), 2407.2288086430635);
   EXPECT_EQ(hist->ValidPixels(), 751066);
   EXPECT_EQ(hist->NullPixels(), 272934);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 0.0012757993805892806);
-  delete hist;
 
-  hist = oddCube->histogram(1);
+  hist.reset(oddCube->histogram(1));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.0012146869906190147);
   EXPECT_DOUBLE_EQ(hist->Sum(), 228.48262293543667);
   EXPECT_EQ(hist->ValidPixels(), 188100);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 2.5492910673775158e-05);
-  delete hist;
 
-  hist = oddCube->histogram(2);
+  hist.reset(oddCube->histogram(2));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.0030423641729513695);
   EXPECT_DOUBLE_EQ(hist->Sum(), 572.26870093215257);
   EXPECT_EQ(hist->ValidPixels(), 188100);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 8.1132094641407011e-05);
-  delete hist;
 
-  hist = oddCube->histogram(3);
+  hist.reset(oddCube->histogram(3));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.0045459914483793511);
   EXPECT_DOUBLE_EQ(hist->Sum(), 849.0366388480179);
   EXPECT_EQ(hist->ValidPixels(), 186766);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 0.00014736498719936585);
-  delete hist;
 
-  hist = oddCube->histogram(4);
+  hist.reset(oddCube->histogram(4));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.0040267987555951965);
   EXPECT_DOUBLE_EQ(hist->Sum(), 757.44084592745639);
   EXPECT_EQ(hist->ValidPixels(), 188100);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 0.00014715790870523248);
-  delete hist;
 
-  hist = oddCube->histogram(5);
+  hist.reset(oddCube->histogram(5));
   EXPECT_DOUBLE_EQ(hist->Average(), -1.7976931348623149e+308);
   EXPECT_DOUBLE_EQ(hist->Sum(), 0);
   EXPECT_EQ(hist->ValidPixels(), 0);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), -1.7976931348623149e+308);
-  delete hist;
 
-  hist = evenCube->histogram(0);
+  hist.reset(evenCube->histogram(0));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.0031812268601306222);
   EXPECT_DOUBLE_EQ(hist->Sum(), 125.25762639078312);
   EXPECT_EQ(hist->ValidPixels(), 39374);
   EXPECT_EQ(hist->NullPixels(), 984626);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 0.0011403444106711229);
-  delete hist;
 
-  hist = evenCube->histogram(1);
+  hist.reset(evenCube->histogram(1));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.0012088832911103964);
   EXPECT_DOUBLE_EQ(hist->Sum(), 9.5743556655943394);
   EXPECT_EQ(hist->ValidPixels(), 7920);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 1.9940037117194649e-05);
-  delete hist;
 
-  hist = evenCube->histogram(2);
+  hist.reset(evenCube->histogram(2));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.0030467073489838948);
   EXPECT_DOUBLE_EQ(hist->Sum(), 24.129922203952447);
   EXPECT_EQ(hist->ValidPixels(), 7920);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 6.7849469699949677e-05);
-  delete hist;
 
-  hist = evenCube->histogram(3);
+  hist.reset(evenCube->histogram(3));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.0045480986605348729);
   EXPECT_DOUBLE_EQ(hist->Sum(), 34.993071094155312);
   EXPECT_EQ(hist->ValidPixels(), 7694);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 0.00011021847268104142);
-  delete hist;
 
-  hist = evenCube->histogram(4);
+  hist.reset(evenCube->histogram(4));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.0040220504221235957);
   EXPECT_DOUBLE_EQ(hist->Sum(), 31.854639343218878);
   EXPECT_EQ(hist->ValidPixels(), 7920);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 0.00011120636199309424);
-  delete hist;
 
-  hist = evenCube->histogram(5);
+  hist.reset(evenCube->histogram(5));
   EXPECT_DOUBLE_EQ(hist->Average(), 0.0031193987479623915);
   EXPECT_DOUBLE_EQ(hist->Sum(), 24.705638083862141);
   EXPECT_EQ(hist->ValidPixels(), 7920);
   EXPECT_DOUBLE_EQ(hist->StandardDeviation(), 3.7732129681642477e-05);
-  delete hist;
 }
